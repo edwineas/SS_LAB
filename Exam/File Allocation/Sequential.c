ad156162cec5993c10fb5693e94b7de1d064f6e5
edwineas <edwinalexshaji@gmail.com>
2023-12-09 17:39:07 +0530

    Update Sequential.c

--- Exam/File Allocation/Sequential.c
+++ Exam/File Allocation/Sequential.c
@@ -3,15 +3,15 @@
 void main() {
     int n, i, j, b[20], sb[20], t, x, c[20][20];
     printf("Enter no. of files:");
-    scanf("%d", &n); // n is the number of files
-    for (i = 0; i < n; i++) { // Loop through each file
+    scanf("%d", &n);
+    for (i = 0; i < n; i++) {
         printf("Enter no. of blocks occupied by file %d:", i + 1);
-        scanf("%d", &b[i]); // b[i] is the number of blocks occupied by file i+1
+        scanf("%d", &b[i]);
         printf("Enter the starting block of file %d:", i + 1); 
-        scanf("%d", &sb[i]); // sb[i] is the starting block of file i+1
-        t = sb[i]; // t[i] is the starting block of file i+1
+        scanf("%d", &sb[i]);
+        t = sb[i];
         for (j = 0; j < b[i]; j++) {
-            c[i][j] = t++; // c[i][j] is the block number of the jth block of file i+1
+            c[i][j] = t++;
         }
     }
     
